6461f3f23ad29fc53be6444a686ff3ebbd337b4f
onceinteractive <nolan@onceinteractive.com>
2015-09-19 21:27:11 +0800

    Initiate app

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,35 +1,21 @@
-angular.module('onceinteractive.controllers', ['onceinteractive.services'])
+angular.module('onceinteractive.controllers')
  
 .controller('HomeCtrl', function ($rootScope, $scope, API, $window) {
-    // if the user is already logged in, take him to his bucketlist
-    if ($rootScope.isSessionActive()) {
-        $window.location.href = ('#/bucket/list');
-    }
- 
-    $scope.user = {
-        email: "",
-        password: ""
-    };
- 
-    $scope.validateUser = function () {
-        var email = this.user.email;
-        var password = this.user.password;
-        if(!email || !password) {
-            $rootScope.notify("Please enter valid credentials");
-            return false;
+    $scope.message = 'test';
+   
+    $scope.scotches = [
+        {
+            name: 'Macallan 12',
+            price: 50
+        },
+        {
+            name: 'Chivas Regal Royal Salute',
+            price: 10000
+        },
+        {
+            name: 'Glenfiddich 1937',
+            price: 20000
         }
-        $rootScope.show('Please wait.. Authenticating');
-        API.signin({
-            email: email,
-            password: password
-        }).success(function (data) {
-            $rootScope.setToken(email); // create a session kind of thing on the client side
-            $rootScope.hide();
-            $window.location.href = ('#/bucket/list');
-        }).error(function (error) {
-            $rootScope.hide();
-            $rootScope.notify("Invalid Username or password");
-        });
-    }
+    ];
  
 })
